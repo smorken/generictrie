98f1ecf277d07663abf9342c34d65f401ac25834
scottm361@gmail.com <scottm361@gmail.com@8456a6e0-eb77-11de-bdfd-b3e7e4bb8034>
2010-08-21 06:18:12 +0000

    implemented prefix search

--- trunk/GenericTrie/Program.cs
+++ trunk/GenericTrie/Program.cs
@@ -12,20 +12,20 @@ namespace GenericTrie
  
         static void Main(string[] args)
         {
-            Trie<string, char, int> Trie = new Trie<string, char, int>();
+            Trie<string, char, int> Trie = new Trie<string, char, int>(NodeContainerType.SortedDictionary);
             Trie.Add("ABCD", 1);
             Trie.Add("ABED", 2);
             Trie.Add("ACED", 4);
+            Trie.Add("BCED", 4);
             Trie.WildCard = '?';
             List<int> value = Trie["?B??"];
-            List<int> value2 = Trie["A"];
-            IntTrieTest test1 = new IntTrieTest(NodeContainerType.Dictionary, 5000, 5, 100, 5200);
-
+            List<char[]> value2 = Trie.PrefixKeySearch("???D");
+            List<int> vla = Trie.PrefixValueSearch("?");
+            IntTrieTest test1 = new IntTrieTest(NodeContainerType.Dictionary, 5000, 5, 100, 26);
             string values = test1.Start();
 
-            //IntTrieTest test2 = new IntTrieTest(NodeContainerType.Dictionary, 500000, 1, 10, 500);
-
-            //string values2 = test2.Start();
+            IntTrieTest test2 = new IntTrieTest(NodeContainerType.Dictionary, 500000, 1, 10, 500);
+            string values2 = test2.Start();
 
             StringTrieTest test3 = new StringTrieTest(NodeContainerType.LinearAssociativeArray, 50000, 2, 10, 26);
             string derp = test3.Start();
